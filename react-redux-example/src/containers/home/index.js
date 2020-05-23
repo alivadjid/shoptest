ffa903342ee5e8855b81efdaa413f9fc1d395a83
alivadjid1 <alivadjid@gmail.com>
2020-05-23 20:07:17 +0300

    modified src/containers/home/index.js

--- react-redux-example/src/containers/home/index.js
+++ react-redux-example/src/containers/home/index.js
@@ -2,20 +2,43 @@ import React from 'react'
 import { push } from 'connected-react-router'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import {
+	increment,
+	incrementAsync,
+	decrement,
+	decrementAsync
+} from '../../modules/counter'
 
 const Home = props => (
 	<div>
 		<h1>Home</h1>
 		<p>Welcome home!</p>
-		<button onClick={() => props.chanhePage()}> Go to about page via redux</button>
+		<p>Count: {props.count}</p>
+
+		<p>
+			<button onClick={props.increment}>Increment</button>
+			<button onClick={props.decrementAsync} disabled={props.isDecrementing}>Decrement Async</button>
+		</p>
+
+		<p><button onClick={() => props.chanhePage()}> Go to about page via redux</button></p>
 	</div>
 )
 
+const mapStateToProps = ({ counter }) => ({
+	count: counter.count,
+	isIncrementing: counter.isIncrementing,
+	isDecrementing: counter.isDecrementing
+})
+
 const mapDispatchToProps = dispatch => bindActionCreators({
+	increment,
+	incrementAsync,
+	decrement,
+	decrementAsync,
 	changePage: () => push('/about-us')
 }, dispatch)
 
 export default connect(
-	null,
+	mapStateToProps,
 	mapDispatchToProps
 )(Home)
